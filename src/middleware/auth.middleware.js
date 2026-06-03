2915ef3b4c51edd900708fb59a88c90d2bd5a1d5
Vijay-Yevale <yevalevijay0002@gmail.com>
2026-06-03 17:58:19 +0530

    feat:auth complete with middleware verified

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,23 +1,28 @@
+const User = require("../models/User.model.js");   
 const AppError = require("../utils/AppError");
-const { verifyToken } = require("../utils/jwt.util");  
+const { verifyToken } = require("../utils/jwt.util");
 
-const authMiddleware = (req, res, next) => {           
-  const authHeader = req.headers.authorization;         
+const authMiddleware = async (req, res, next) => { 
+  const authHeader = req.headers.authorization;
 
-  if (!authHeader) {                                    
+  if (!authHeader) {
     return next(new AppError("No token provided", 401));
   }
 
-  const token = authHeader.split(" ")[1];               
+  const token = authHeader.split(" ")[1];
 
-  const decoded = verifyToken(token);                  
-  if (!decoded) {                                       
+  const decoded = verifyToken(token);
+  if (!decoded) {
     return next(new AppError("Invalid token", 401));
   }
 
-  req.user = decoded;                                  
-  next();                                              
+  const user = await User.findById(decoded.id);    
+  if (!user) {
+    return next(new AppError("User not found", 401));
+  }
+
+  req.user = user;                               
+  next();
 };
 
-module.exports = authMiddleware;                        
-                                                       
+module.exports = authMiddleware;
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const authRouter = express.Router();
 const { register, login } = require("./auth.controller.js");
 const validate = require("../../middleware/validate.middleware.js");
+const authMiddleware = require("../../middleware/auth.middleware.js"); 
 const { registerSchema, loginSchema } = require("./auth.validator.js");
-
+const allowOnly = require("../../middleware/role.middleware.js");
 
 authRouter.post("/register", validate(registerSchema), register);
 authRouter.post("/login", validate(loginSchema), login);
+
+
 
 module.exports = authRouter;
